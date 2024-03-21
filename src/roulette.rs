3832ba3ac4bcf48784b3b369f921ba01c339898a
brage-andreas <draaangooo@gmail.com>
2024-03-21 01:56:15 +0100

    refactor: remove `JsonFile` and `.cloned()`

--- src/roulette.rs
+++ src/roulette.rs
@@ -2,11 +2,6 @@ use lazy_static::lazy_static;
 use rand::seq::SliceRandom;
 use serde::{de::DeserializeOwned, Deserialize};
 
-enum JsonFile {
-    FortunePhrase,
-    Pizza,
-}
-
 pub enum RouletteFilter {
     All,
     Vegan,
@@ -33,42 +28,36 @@ impl Deserializable for PizzaEntry {}
 impl Deserializable for FortunePhraseEntry {}
 
 lazy_static! {
-    static ref PIZZAS: Vec<PizzaEntry> = load_from_json(JsonFile::Pizza);
-    static ref FORTUNE_PHRASES: Vec<FortunePhraseEntry> = load_from_json(JsonFile::FortunePhrase);
+    static ref PIZZAS: Vec<PizzaEntry> = load_json(include_str!("../config/pizzas.json"));
+    static ref FORTUNE_PHRASES: Vec<FortunePhraseEntry> =
+        load_json(include_str!("../config/fortune_phrases.json"));
 }
 
-fn load_from_json<T: Deserializable>(file: JsonFile) -> Vec<T> {
-    let json = match file {
-        JsonFile::FortunePhrase => include_str!("../config/fortune_phrases.json"),
-        JsonFile::Pizza => include_str!("../config/pizzas.json"),
-    };
-
-    serde_json::from_str(json).expect("Failed to parse JSON configuration")
+fn load_json<T: Deserializable>(include_str: &str) -> Vec<T> {
+    serde_json::from_str(include_str).expect("Failed to parse JSON configuration")
 }
 
-fn get_random_element<T>(source: Vec<T>) -> Option<T>
+fn get_random_element<T>(source: &[T]) -> Option<&T>
 where
     T: Clone,
 {
     let mut rng = rand::thread_rng();
-    source.choose(&mut rng).cloned()
+    source.choose(&mut rng)
 }
 
-pub fn get_random_pizza(filter: RouletteFilter) -> PizzaEntry {
-    let filtered_pizzas: Vec<PizzaEntry> = PIZZAS
+pub fn get_random_pizza<'a>(filter: RouletteFilter) -> &'a PizzaEntry {
+    let filtered_pizzas: Vec<&PizzaEntry> = PIZZAS
         .iter()
         .filter(|pizza_entry| match filter {
             RouletteFilter::All => true,
             RouletteFilter::Vegan => pizza_entry.vegan,
             RouletteFilter::Vegetarian => pizza_entry.vegetarian,
         })
-        .cloned()
         .collect();
 
-    get_random_element(filtered_pizzas).expect("PizzaEntry vector is empty")
+    get_random_element(&filtered_pizzas).expect("PizzaEntry vector is empty")
 }
 
-pub fn get_random_fortune_phrase() -> FortunePhraseEntry {
-    let phrases = FORTUNE_PHRASES.to_vec();
-    get_random_element(phrases).expect("FortunePhrases vector is empty")
+pub fn get_random_fortune_phrase<'a>() -> &'a FortunePhraseEntry {
+    get_random_element(&FORTUNE_PHRASES).expect("FortunePhrases vector is empty")
 }

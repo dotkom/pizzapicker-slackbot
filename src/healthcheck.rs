d251866ba0c6b44527ff1e41425e8ff3c166b9f6
Mats Larsen <mats@jun.codes>
2024-04-01 20:15:01 +0200

    Bind to 0.0.0.0

--- src/healthcheck.rs
+++ src/healthcheck.rs
@@ -5,7 +5,7 @@ use tokio::net::TcpListener;
 #[tracing::instrument]
 pub async fn start_http_server() -> tokio::io::Result<()> {
     tracing::info!("Starting HTTP server");
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let listener = TcpListener::bind(addr).await?;
 
     loop {
